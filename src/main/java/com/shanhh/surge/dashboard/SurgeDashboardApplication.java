eb87cf807a1ad3b1416a97976744d371d64d46cf
Honghao SHAN 单弘昊 <honghao.shan@nio.com>
2022-12-30 20:45:17 +0800

    feature init a empty project

---
+++ src/main/java/com/shanhh/surge/dashboard/SurgeDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.shanhh.surge.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurgeDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurgeDashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shanhh/surge/dashboard/SurgeDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shanhh.surge.dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurgeDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
